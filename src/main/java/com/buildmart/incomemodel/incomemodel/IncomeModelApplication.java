310a8e4b41f648460a30d6eb4776358a201d798d
Faizan Ahmed <faizanahmed@Faizans-MacBook-Air.local>
2023-08-16 19:41:54 +0530

    First Commit | Enabled git

---
+++ src/main/java/com/buildmart/incomemodel/incomemodel/IncomeModelApplication.java
@@ -0,0 +1,13 @@
+package com.buildmart.incomemodel.incomemodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomeModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncomeModelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buildmart/incomemodel/incomemodel/IncomeModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildmart.incomemodel.incomemodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomeModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
